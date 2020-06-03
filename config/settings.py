57d17df379edee868581124ea6093b1ad0ed49ca
shervinmo <abtin@riseup.net>
2020-06-03 22:18:49 +0430

    change project structure
    http://cookiecutter-django.readthedocs.io

--- config/settings.py
+++ config/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
+ROOT_URLCONF = "config.urls"
+# https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
+WSGI_APPLICATION = "config.wsgi.application"
