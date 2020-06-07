52cfb394421ada714d9adcea4e0c0ece7659e7b8
shervinmo <abtin@riseup.net>
2020-06-08 00:47:54 +0430

    add auto schema

--- dideban/user/api/v1/rest/serializers.py
+++ dideban/user/api/v1/rest/serializers.py
@@ -3,7 +3,22 @@ from dideban.user.models import User
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    email = serializers.ReadOnlyField()
+    date_joined = serializers.SerializerMethodField(read_only=True)
+    last_login = serializers.SerializerMethodField(read_only=True)
+    is_staff = serializers.ReadOnlyField()
 
     class Meta:
         model = User
-        fields = ["email", "is_staff", "date_joined"]
+        fields = [
+            "email", "is_staff", "date_joined",
+            "first_name", "last_name", "last_login",
+        ]
+
+    @staticmethod
+    def get_last_login(obj):
+        return obj.last_login.timestamp() if obj.last_login else None
+
+    @staticmethod
+    def get_date_joined(obj):
+        return obj.date_joined.timestamp() if obj.date_joined else None
--- dideban/user/api/v1/rest/views.py
+++ dideban/user/api/v1/rest/views.py
@@ -1,11 +1,33 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import UserProfileSerializer
+from drf_yasg.utils import swagger_auto_schema
+from utils.common.drf_params import jwt_key
 
 
 class UserProfile(APIView):
     permission_classes = [IsAuthenticated]
+    serializer_class = UserProfileSerializer
 
+    @swagger_auto_schema(
+        manual_parameters=[jwt_key],
+        responses={200: UserProfileSerializer})
     def get(self, request):
-        return Response(UserProfileSerializer(instance=request.user).data)
+        return Response(self.serializer_class(instance=request.user).data)
+
+    @swagger_auto_schema(
+        request_body=UserProfileSerializer,
+        manual_parameters=[jwt_key],
+        responses={status.HTTP_200_OK: UserProfileSerializer}
+    )
+    def patch(self, request):
+        serializer = self.serializer_class(
+            request.user,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
